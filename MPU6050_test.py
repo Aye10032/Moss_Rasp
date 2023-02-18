76f433f44a67282b0295df6519c29d678c98b7c6
Aye10032 <aye10032@gmail.com>
2023-02-18 17:24:05 +0800

    Initial commit

---
+++ MPU6050_test.py
@@ -0,0 +1,28 @@
+import time
+
+from mpu6050 import mpu6050
+
+sensor = mpu6050(0x68)
+
+while True:
+    accel_data = sensor.get_accel_data()
+
+    gyro_data = sensor.get_gyro_data()
+
+    print("Accelerometer data")
+
+    print("x: " + str(accel_data['x']))
+
+    print("y: " + str(accel_data['y']))
+
+    print("z: " + str(accel_data['z']))
+
+    print("Gyroscope data")
+
+    print("x: " + str(gyro_data['x']))
+
+    print("y: " + str(gyro_data['y']))
+
+    print("z: " + str(gyro_data['z']))
+
+    time.sleep(1)
